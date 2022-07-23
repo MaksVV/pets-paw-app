f501711f27824c5ad54db91c6635010fcd756704
User <user@superusers-MacBook-Pro.local>
2022-07-23 17:18:50 +0300

    feat: addbreeds images

--- src/components/breeds/breeds-images/BreedsImages.js
+++ src/components/breeds/breeds-images/BreedsImages.js
@@ -1,61 +1,79 @@
 import "./BreedsImages.css";
 
-import image from "./img.png"
+import img from "./images/Savannah.png";
+import img1 from "./images/Bengal.png";
+import img2 from "./images/Kartäuser.png";
+import img3 from "./images/Maycun.png";
+import img4 from "./images/Siamskaya.png";
+import img5 from "./images/Norwen.png";
+import img6 from "./images/Turkish_Angora.png";
+import img7 from "./images/Bombay.png";
+import img8 from "./images/British_Longhair.png";
+import img9 from "./images/Toyger.png";
 
 const BreedsImages = () => {
     return (
         <> 
             <div className="images" >
-                <img class="image" src={image} alt="cat" />
+                <img class="image" src={img} alt="cat" />
                 <div className="imageHover">
-                    <p className="textHover">ajhfkajfhak</p>
+                    <p className="textHover">Savannah</p>
                 </div>
             </div>
             <div className="images" >
-                <img class="image" src={image} alt="cat" />
+                <img class="image" src={img1} alt="cat" />
                 <div className="imageHover">
-                    <p className="textHover">ajhfkajfhak</p>
+                    <p className="textHover">Bengal</p>
                 </div>
             </div>
             <div className="images" >
-                <img class="image" src={image} alt="cat" />
+                <img class="image" src={img2} alt="cat" />
                 <div className="imageHover">
-                    <p className="textHover">ajhfkajfhak</p>
+                    <p className="textHover">Kartauser</p>
                 </div>
             </div>
             <div className="images" >
-                <img class="image" src={image} alt="cat" />
+                <img class="image" src={img3} alt="cat" />
                 <div className="imageHover">
-                    <p className="textHover">ajhfkajfhak</p>
+                    <p className="textHover">Maycun</p>
                 </div>
-            </div>    <div className="images" >
-                <img class="image" src={image} alt="cat" />
+            </div>    
+            <div className="images" >
+                <img class="image" src={img4} alt="cat" />
                 <div className="imageHover">
-                    <p className="textHover">ajhfkajfhak</p>
+                    <p className="textHover">Siamskaya</p>
                 </div>
             </div>
             <div className="images" >
-                <img class="image" src={image} alt="cat" />
+                <img class="image" src={img5} alt="cat" />
                 <div className="imageHover">
-                    <p className="textHover">ajhfkajfhak</p>
+                    <p className="textHover">Norwegian</p>
                 </div>
-            </div>    <div className="images" >
-                <img class="image" src={image} alt="cat" />
+            </div>    
+            <div className="images" >
+                <img class="image" src={img6} alt="cat" />
                 <div className="imageHover">
-                    <p className="textHover">ajhfkajfhak</p>
+                    <p className="textHover">Turkish_Angora</p>
                 </div>
             </div>
             <div className="images" >
-                <img class="image" src={image} alt="cat" />
+                <img class="image" src={img7} alt="cat" />
                 <div className="imageHover">
-                    <p className="textHover">ajhfkasjfhak</p>
+                    <p className="textHover">Bombey</p>
                 </div>
-            </div>    <div className="images" >
-                <img class="image" src={image} alt="cat" />
-            </div>
+            </div>    
             <div className="images" >
-                <img class="image" src={image} alt="cat" />
-            </div>
+                <img class="image" src={img8} alt="cat" />
+                <div className="imageHover">
+                    <p className="textHover">British</p>
+                </div>
+            </div>   
+            <div className="images" >
+                <img class="image" src={img9} alt="cat" />
+                <div className="imageHover">
+                    <p className="textHover">Toyger</p>
+                </div>
+            </div>   
         </>
     );
 }
